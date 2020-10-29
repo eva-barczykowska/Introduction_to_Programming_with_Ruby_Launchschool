423526fa3aab73db41ed3aad3ff95e012ec89d42
eva-barczykowska <eva.barczykowska@gmail.com>
2020-10-29 11:28:30 +0100

    if you have an empty directory, it will not be visible on git, created a file inside to make it visible

---
+++ 1_Introduction/intro.rb
@@ -0,0 +1,3 @@
+# if there is file inside, the empty folder is not visible on git, this is interesting
+# I created a file inside the Introduction folder and now I will commit this change it and push it to git
+# 1_Introduction folder should be visible now
